93033c3d48460d5d28103ba949b08f184ca8d114
Niel Ok <nielok@stanford.edu>
2025-04-08 21:02:37 -0700

    mouse fixing

--- cursor.js
+++ cursor.js
@@ -1,8 +1,16 @@
-const cursorDot = document.createElement('div');
-cursorDot.classList.add('cursor-dot');
-document.body.appendChild(cursorDot);
+if (!('ontouchstart' in window)) {
+  const cursorDot = document.createElement('div');
+  cursorDot.classList.add('cursor-dot');
+  cursorDot.style.opacity = "0"; // start hidden
+  document.body.appendChild(cursorDot);
 
-document.addEventListener('mousemove', (e) => {
-  cursorDot.style.left = e.clientX + 'px';
-  cursorDot.style.top = e.clientY + 'px';
-});
+  document.addEventListener('mousemove', (e) => {
+    cursorDot.style.left = e.clientX + 'px';
+    cursorDot.style.top = e.clientY + 'px';
+
+    // Always restore opacity if needed
+    if (cursorDot.style.opacity !== "1") {
+      cursorDot.style.opacity = "1";
+    }
+  });
+}
